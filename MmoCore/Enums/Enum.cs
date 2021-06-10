e67692d7fc02170a4b213bb6fbdd1f582b06e04c
sweetmeatsboy <sweetmeatsboy@gmail.com>
2021-06-10 21:41:13 +0900

    Mmo service에서 사용할 Enum:ContentType 추가.

---
+++ MmoCore/Enums/Enum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MmoCore.Enums
+{
+    public enum CONTENT_TYPE : ushort
+    {
+        NONE,
+        TEST,
+        HB_CHECK,
+        HELLO,
+        WELCOME,
+        CHAT,
+        END,
+    }
+}
