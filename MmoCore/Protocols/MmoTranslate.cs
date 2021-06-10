82923136d9607b1f51f37dec0e517caa533d12c0
sweetmeatsboy <sweetmeatsboy@gmail.com>
2021-06-10 21:49:48 +0900

    CoreNet dll 갱신 및 MMO용 Translate 추가.

---
+++ MmoCore/Protocols/MmoTranslate.cs
@@ -0,0 +1,32 @@
+﻿using CoreNet.Networking;
+using CoreNet.Protocols;
+using MmoCore.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MmoCore.Protocols
+{
+    public static class MmoTranslate
+    {
+        private static bool isInit = false;
+        public static void Init()
+        {
+            if (isInit)
+                return;
+            Translate.RegistCustom<CONTENT_TYPE>((NetStream _s, object _val)
+                => {
+                    _s.WriteInt32((Int32)_val);
+                },
+            (NetStream _s) => {
+                var ret = default(CONTENT_TYPE);
+                ret = (CONTENT_TYPE)_s.ReadInt32();
+                return ret;
+            });
+
+            isInit = true;
+        }
+    }
+}
